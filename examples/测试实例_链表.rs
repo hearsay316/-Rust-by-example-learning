473f46a580a8d2d70a24380b7458b59158b52c0d
hearsay316 <hearsay316@gmail.com>
2024-07-26 22:03:13 +0800

    feat: 测试实例_链表

---
+++ examples/测试实例_链表.rs
@@ -0,0 +1,38 @@
+use List::*;
+
+enum List {
+    Cons(u32, Box<List>),
+    Nil,
+}
+impl List {
+    fn new() -> List {
+        Nil
+    }
+    fn preend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{},{}", head, tail.stringify())
+            }
+            Nil => {
+                "Nil".to_string()
+            }
+        }
+    }
+}
+fn main() {
+    let mut list = List::new();
+    list = list.preend(1);
+    list = list.preend(2);
+    list = list.preend(3);
+    println!("linked list has length:{}", list.len());
+    println!("{}", list.stringify());
+}
