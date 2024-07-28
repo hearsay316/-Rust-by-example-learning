cd61580c684c5a5a41823fe203696d8a30859bfe
hearsay316 <hearsay316@gmail.com>
2024-07-28 21:40:06 +0800

    feat: 可变变量

---
+++ examples/可变变量.rs
@@ -0,0 +1,10 @@
+// 变量绑定默认是不可变的 ,但是加上mut 修饰语句后变量就可以改变
+fn main(){
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+    println!("Before mutation:{}", mutable_binding);
+    mutable_binding+=1;
+    println!("After mutation :{}",mutable_binding);
+    // _immutable_binding+=1;
+    println!("{}",_immutable_binding);
+}
