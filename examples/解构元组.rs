67c34181f9492b284c4202b475be7553f4c2e185
hearsay316 <hearsay316@gmail.com>
2024-07-30 16:39:17 +0800

    feat: 解构元组

---
+++ examples/解构元组.rs
@@ -0,0 +1,23 @@
+// 元组 可以在match 中解构,如下所示
+fn main (){
+    /*
+    letletletleltletletletleltletletleltletletlletletleltletletleltletllteletllletleltletletletlletletlleltelteltletletleltletlletlletletlletletleltletletletletlelttlelt
+
+    let let let; let let let;let let let let let let let let letletletlet
+
+    */
+    let  triple = (0,-2,3);
+    println!("Tell me about :{:?}",triple);
+    match triple {
+        (0,x,y)=>{
+            println!("第一个元素是:{},  第二个元素是{} , ",x,y);
+        },
+        (1,..)=>{
+            println!(" 这个是计算啥");
+
+        },
+        _=>{
+        println!(" 这个是最后的else")
+        }
+    }
+}
