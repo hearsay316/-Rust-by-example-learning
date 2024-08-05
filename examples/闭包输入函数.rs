6f897e77f1a92c16e4976e6d59c0ef579ad86a44
hearsay316 <hearsay316@gmail.com>
2024-08-05 20:24:15 +0800

    feat: 闭包作为输入参数

---
+++ examples/闭包输入函数.rs
@@ -0,0 +1,13 @@
+fn call_me<F: Fn()>(f: F) {
+    f()
+}
+
+fn function() {
+    println!("I'm a function");
+}
+
+fn main() {
+    let closure = || println!("I', a closure");
+    call_me(closure);
+    call_me(function);
+}
