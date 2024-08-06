2fd01febdc069148ae301bb7e3631887226b0e22
hearsay316 <hearsay316@gmail.com>
2024-08-06 23:52:31 +0800

    feat: 函数的学习

---
+++ examples/std中的例子2.rs
@@ -0,0 +1,23 @@
+// pub trait Iterator {
+//     type Item;
+//     fn find<P> (&mut self ,predicate:P)->Option<Self::Item> where p:FnMut(&Self::Item)->bool{
+//
+//     }
+// }
+
+
+fn main() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    let mut iter = vec1.iter();
+
+    let mut into_iter = vec2.into_iter();
+    println!("Find 2 in vec1:{:?}", iter.find(|&&x| x == 2).unwrap());
+    println!("Find 2 in vec2:{:?}", into_iter.find(|&x| x == 2));
+    // println!("{:?}",vec2.into_iter());
+    let array1 = ["1","2", "3"];
+    let array2 = ["44","54", "26"];
+    println!("Find 2 in array1:{:?}",array1.iter().find(|&x|*x=="2"));
+    println!("Find 2 in array2:{:?}", array2.into_iter().find(|&x|x =="2"));
+    // println!("{}",(array2.into_iter()) == (array2.into_iter()));
+}
---
+++ examples/闭包发散函数.rs
@@ -0,0 +1,31 @@
+fn foo() -> ! {
+    panic!("This call never returns.");
+}
+fn some_fn() {
+    ()
+}
+
+// fn main(){
+//
+//     let a:() = some_fn();
+//     println!("This function returns and you can see this line.{:?}", a);
+// }
+
+fn main() {
+    // let x:! = panic!("This call never returns.");
+    // println!("You will never see this line!")
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+        for i in 0..up_to {
+            let addition: u32 = match i % 2 == 1 {
+                true => i,
+                false => continue,
+            };
+            println!("{}",addition);
+            acc += addition;
+        }
+        acc
+    }
+    println!("Sum of add numbers up to 9 (excluding):{}",sum_odd_numbers(9));
+    println!("{}",1%2);
+}
