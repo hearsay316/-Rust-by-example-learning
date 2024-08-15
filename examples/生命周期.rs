354b0b88ae7e7141bde2037f423aa265f00b5df0
hearsay316 <hearsay316@gmail.com>
2024-08-16 00:26:05 +0800

    feat: 生命周期

---
+++ examples/生命周期.rs
@@ -0,0 +1,12 @@
+
+fn main(){
+    let i =3;
+    {
+        let borrow1 =&1;
+        println!("borrow1:{}",borrow1);
+    }
+    {
+        let borrow2 = &i;
+        println!("borrow2 :{}",borrow2);
+    }
+}
---
+++ examples/生命周期显式标注.rs
@@ -0,0 +1,13 @@
+fn  print_refs<'a,'b>(x:&'a i32,y:&'b i32){
+    println!("x is {} and y is {}", x,y);
+}
+fn  failed_borrow<'a>(){
+    let _x = 12;
+    // let y :&'a i32 = &_x;
+    println!("{}",_x);
+}
+fn  main (){
+    let  (four,nine) = (4,9);
+    print_refs(&four,&nine);
+    failed_borrow();
+}
