a056c6780da503ecced8a31d3f3728afe1e8c1a3
hearsay316 <hearsay316@gmail.com>
2024-08-19 23:21:25 +0800

    feat: 错误处理 Option

---
+++ examples/错误处理panic.rs
@@ -0,0 +1,10 @@
+fn main(){
+    give_princess("teddy bear");
+    give_princess("snake");
+
+}
+fn  give_princess(gift:&str){
+    if gift=="snake" { panic!("AAAaaaaa");}
+
+    println!("I love {}s !!!!",gift);
+}
