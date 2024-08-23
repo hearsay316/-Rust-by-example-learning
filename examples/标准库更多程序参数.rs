afc3a0555312e99d49da0f9871f96587bea5e586
hearsay316 <hearsay316@gmail.com>
2024-08-23 14:59:54 +0800

    feat: 学习 标准库更多

---
+++ examples/标准库更多程序参数.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main(){
+let args :Vec<String> = env::args().collect();
+    println!("My path os {:?}.",args);
+
+    println!("I got {:?} arguments:{:?}",args.len() -1 ,&args[1..]);
+
+
+}
