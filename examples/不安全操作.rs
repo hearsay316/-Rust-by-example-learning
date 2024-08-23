0ece2cffe8fa0eb032c42b59e0bf70f9c49dd5f5
hearsay316 <hearsay316@gmail.com>
2024-08-23 15:20:36 +0800

    feat: 不安全操作

---
+++ examples/不安全操作.rs
@@ -0,0 +1,16 @@
+use std::slice;
+
+fn main(){
+    let raw_p:*const u32 = &10;
+    unsafe{
+        assert!(*raw_p==10);
+    }
+
+    let some_vertor = vec![1,2,3,4];
+    let pointer  =some_vertor.as_ptr();
+    let length = some_vertor.len();
+    unsafe{
+        let my_slice:&[u32] = slice::from_raw_parts(pointer,length);
+        assert_eq!(some_vertor.as_slice(),my_slice)
+    }
+}
