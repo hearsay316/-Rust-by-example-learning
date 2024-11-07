81add15cca40cce9fc92d8b8c4d9790672e142f0
hearsay316 <hearsay316@gmail.com>
2024-11-07 18:38:08 +0800

    feat(examples): 添加读取目录修改文件后缀的 CLI 工具
    - 新增一个 Rust程序，用于批量修改指定目录下文件的后缀
    - 支持通过命令行参数指定目录路径和新后缀
    - 如果没有提供目录路径参数，默认使用当前工作目录
    - 如果没有提供后缀参数，默认将文件后缀修改为 "mp4"
    -程序会遍历指定目录下的所有文件，并修改它们的后缀
    - 添加了错误处理和日志输出，以提高程序的健壮性和可读性

---
+++ examples/读取目录修改文件后缀cli工具.rs
@@ -0,0 +1,60 @@
+use std::path::PathBuf;
+use std::{env, fs};
+
+#[warn(unused)]
+#[derive(Debug)]
+struct Options {
+    path: String,   // 文件路径
+    suffix: String, // 后缀格式
+}
+impl Options {
+    fn new(args: Vec<String>) -> Self {
+        // 获取当前工作目录
+        let current_dir = env::current_dir().expect("获取当前目录失败");
+        // 将 PathBuf 转换为字符串
+        let current_dir_str = current_dir.to_string_lossy().to_string();
+        println!("当前目录 :{:?}", current_dir_str);
+        let path = args.get(1).unwrap_or_else(|| &current_dir_str).clone();
+        let suffix = args.get(2).unwrap_or(&"mp4".to_string()).clone();
+        Options { path, suffix }
+    }
+    // 修改文件夹后缀
+    fn update_name_suffix(&self) {
+        if get_path_file(&self.path) {
+            println!("输入的是个文件");
+            update_name_suffix(PathBuf::from(&self.path), &self.suffix);
+            println!("修改完成");
+            return;
+        }
+        let entries = fs::read_dir(&self.path).expect("读取文件夹失败");
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let file_path = entry.path();
+                if file_path.is_file() {
+                    update_name_suffix(file_path, &self.suffix);
+                }
+            }
+        }
+        println!("修改完成");
+    }
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let options = Options::new(args);
+    println!("数据是: {:?}", options);
+    options.update_name_suffix();
+}
+
+fn get_path_file(path: &str) -> bool {
+    let metadata = fs::metadata(path).expect("路径不存在");
+    metadata.is_file()
+}
+fn update_name_suffix(entry: PathBuf, suffix: &str) {
+    let mut ren_name = entry.clone();
+    let old_name = entry.clone();
+    ren_name.set_extension(&suffix);
+    println!("{:?}", ren_name);
+    println!("正在修改文件名称,{:?},{:?},{}", ren_name, old_name, suffix);
+    fs::rename(old_name, ren_name).expect("改名字失败");
+    println!("修改 {:?} 成功", entry);
+}
