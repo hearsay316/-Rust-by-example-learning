5baecd6bafecf5ffb5c9d375047ee29eccb526bf
hearsay316 <hearsay316@gmail.com>
2024-11-10 17:05:04 +0800

    refactor(examples): 重构目录和文件名后缀修改功能- 重命名 update_name_suffix函数为 update_file_name_suffix，提高代码可读性 - 新增测试用例，覆盖目录和文件名后缀修改功能 - 添加创建测试目录和文件、删除测试目录的辅助函数 - 优化代码结构，提高可维护性和可测试性

--- examples/读取目录修改文件后缀cli工具.rs
+++ examples/读取目录修改文件后缀cli工具.rs
@@ -19,7 +19,7 @@ impl Options {
     // 修改文件夹后缀
     fn update_name_suffix(&self) {
         if get_path_file(&self.path) {
-            update_name_suffix(PathBuf::from(&self.path), &self.suffix);
+            update_file_name_suffix(PathBuf::from(&self.path), &self.suffix);
         } else {
             update_path_name_suffix(&self.path, &self.suffix);
         }
@@ -32,7 +32,7 @@ fn update_path_name_suffix(path: &str, suffix: &str) {
         if let Ok(entry) = entry {
             let file_path = entry.path();
             if file_path.is_file() {
-                update_name_suffix(file_path, suffix);
+                update_file_name_suffix(file_path, suffix);
             } else {
                 let file_path = file_path.to_string_lossy().to_string();
                 update_path_name_suffix(&file_path, suffix);
@@ -44,7 +44,7 @@ fn get_path_file(path: &str) -> bool {
     let metadata = fs::metadata(path).expect("路径不存在");
     metadata.is_file()
 }
-fn update_name_suffix(entry: PathBuf, suffix: &str) {
+fn update_file_name_suffix(entry: PathBuf, suffix: &str) {
     let mut ren_name = entry.clone();
     let old_name = entry.clone();
     ren_name.set_extension(&suffix);
@@ -63,15 +63,26 @@ fn main() {
 mod test {
     use super::*;
     #[test]
-    fn test_update_name_suffix() {
-        let path = "D:\\video\\2023-05-01\\1.mp4";
-        let suffix = "mp4";
-        update_name_suffix(PathBuf::from(path), suffix);
+    fn test_get_path_file() {
+        let path_dir = "./test1";
+        let path_file = "1.mp4";
+        let path = format!("{path_dir}/{path_file}");
+        create_dir_files(path_dir, vec![&path_file]);
+        assert_eq!(get_path_file(&path), true);
+        delete_dir(path_dir);
     }
     #[test]
-    fn test_get_path_file() {
-        let path = "D:\\video\\2023-05-01\\1.mp4";
-        assert_eq!(get_path_file(path), true);
+    fn test_update_file_name_suffix() {
+        let path_dir = "./test2";
+        let path_file = "1";
+        let old_suffix = "mp3";
+        let new_suffix = "mp4";
+        let path = format!("{path_dir}/{path_file}.{old_suffix}");
+        let new_path = format!("{path_dir}/{path_file}.{new_suffix}");
+        create_dir_files(path_dir, vec![&format!("{path_file}.{old_suffix}")]);
+        update_file_name_suffix(PathBuf::from(path), new_suffix);
+        assert_eq!(get_path_file(&new_path), true);
+        delete_dir(path_dir);
     }
     #[test]
     fn test_options_new() {
@@ -84,5 +95,59 @@ mod test {
         println!("数据是: {:?}", options);
         assert_eq!(options.path, "D:\\video\\2023-05-01\\1.mp4".to_string());
         assert_eq!(options.suffix, "mp4".to_string());
+    }
+
+    #[test]
+    fn test_update_path_name_suffix() {
+        let path_dir = "./test3";
+        let path_file = "1";
+        let old_suffix = "mp3";
+        let new_suffix = "mp4";
+        let new_path = format!("{path_dir}/{path_file}.{new_suffix}");
+        create_dir_files(path_dir, vec![&format!("{path_file}.{old_suffix}")]);
+        update_path_name_suffix(&path_dir, new_suffix);
+        assert_eq!(get_path_file(&new_path), true);
+        delete_dir(path_dir);
+    }
+
+    #[test]
+    fn test_update_name_suffix() {
+        let path_dir = "./test4";
+        let path_file = "1";
+        let old_suffix = "mp3";
+        let new_suffix = "mp4";
+        let path = format!("{path_dir}/{path_file}.{old_suffix}");
+        let new_path = format!("{path_dir}/{path_file}.{new_suffix}");
+        let args: Vec<String> = vec!["".to_string(), path.clone(), new_suffix.to_string()];
+        create_dir_files(path_dir, vec![&format!("{path_file}.{old_suffix}")]);
+        let options = Options::new(args);
+        println!("数据是: {:?}", options);
+        options.update_name_suffix();
+        assert_eq!(get_path_file(&new_path), true);
+        delete_dir(path_dir);
+    }
+    fn create_dir_files(path_dir: &str, file_vec: Vec<&str>) {
+        let dir_path = path_dir;
+        // 尝试创建目录
+        match fs::create_dir_all(dir_path) {
+            Ok(_) => println!("目录创建成功或已存在"),
+            Err(e) => println!("创建目录时发生错误: {:?}", e),
+        }
+        if file_vec.is_empty() {
+            return;
+        }
+        for file_name in file_vec {
+            let file_path = format!("{}/{}", dir_path, file_name);
+            match fs::File::create(&file_path) {
+                Ok(_) => println!("文件创建成功"),
+                Err(e) => println!("创建文件时发生错误: {:?}", e),
+            }
+        }
+    }
+    fn delete_dir(path: &str) {
+        match fs::remove_dir_all(path) {
+            Ok(_) => println!("删除目录成功"),
+            Err(e) => println!("删除目录时发生错误: {:?}", e),
+        }
     }
 }
