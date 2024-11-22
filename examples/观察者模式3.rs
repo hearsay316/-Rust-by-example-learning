ce8d882c2a4f2772b122a00240b77bc3f294d1ae
hearsay316 <hearsay316@gmail.com>
2024-11-22 19:32:51 +0800

    feat(examples): 添加观察者模式示例
    - 新增观察者模式示例代码，实现并发计算功能
    - 示例中包含两个结构体：BingFa 和 BingFaV2
    - 实现了多线程并发计算和数据共享- 示例展示了如何使用消息传递实现异步任务

---
+++ examples/观察者模式3.rs
@@ -0,0 +1,203 @@
+use std::fmt::Debug;
+use std::marker::Send;
+use std::ops::Add;
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::{fmt, thread};
+
+pub struct BingFa<T>
+where
+    T: Add<Output = T> + Copy + Send,
+{
+    vec: Vec<T>,
+    pub sum: Arc<Mutex<T>>,
+    channel_number: i32,
+    pub history: Vec<T>,
+}
+
+impl<T> Debug for BingFa<T>
+where
+    T: Add<Output = T> + Copy + Send + Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("BingFa")
+            .field("sum", &self.sum.lock().unwrap().clone())
+            .field("channel_number", &self.channel_number)
+            .field("history", &self.history)
+            .finish()
+    }
+}
+impl<T> BingFa<T>
+where
+    T: Add<Output = T> + Copy + Send + Default + 'static + Debug,
+{
+    pub fn get_vec_channel(&self) -> i32 {
+        self.channel_number
+    }
+    pub fn get_vec(&self) -> Vec<T> {
+        self.vec.clone()
+    }
+    pub fn get_sum(&self) -> T {
+        self.sum.lock().unwrap().clone()
+    }
+    pub fn new(vec: Vec<T>, channel_number: i32) -> Self {
+        let mut bing_fa = Self {
+            vec,
+            sum: Arc::new(Mutex::new(T::default())),
+            channel_number,
+            history: vec![],
+        };
+        BingFa::run_bing_fa(&mut bing_fa);
+        bing_fa
+    }
+    fn run_bing_fa(&mut self) {
+        loop {
+            let mut handles = vec![];
+            let mut channel_number_clone = self.channel_number;
+            if self.vec.len() < self.channel_number as usize {
+                channel_number_clone = self.vec.len() as i32;
+            }
+            let vec_first_10: Vec<T> = self.vec.drain(0..channel_number_clone as usize).collect();
+            for i in vec_first_10.clone() {
+                let sum = self.sum.clone();
+                let handle = thread::spawn(move || {
+                    let mut total_sum = sum.lock().unwrap();
+                    *total_sum = *total_sum + i;
+                });
+                handles.push(handle);
+            }
+            for handle in handles {
+                handle.join().unwrap();
+            }
+            if self.vec.is_empty() {
+                break;
+            }
+        }
+    }
+    pub fn set_data_vec(&mut self, vec_change: Vec<T>) {
+        self.vec.extend(vec_change);
+        self.run_bing_fa();
+    }
+    pub fn clear(&mut self) {
+        let mut current_sum = self.sum.lock().unwrap();
+        self.history.push(current_sum.clone());
+        *current_sum = T::default();
+    }
+}
+
+pub struct BingFaV2<T>
+where
+    T: Add<Output = T> + Copy + Send,
+{
+    bing_fa: BingFa<T>,
+    tx: mpsc::Sender<Vec<T>>,
+    json_hand: thread::JoinHandle<()>,
+}
+
+impl<T> Debug for BingFaV2<T>
+where
+    T: Add<Output = T> + Copy + Send + Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("BingFaV2")
+            .field("sum", &self.bing_fa)
+            .finish()
+    }
+}
+
+impl<T> BingFaV2<T>
+where
+    T: Add<Output = T> + Copy + Send + Default + 'static + Debug,
+{
+    pub fn new(vec: Vec<T>, channel_number: i32) -> Self {
+        let (tx, rx) = mpsc::channel::<Vec<T>>();
+        let mut bing_fa = BingFa::new(vec, channel_number);
+
+        let sum = bing_fa.sum.clone();
+        let json_hand = thread::spawn(move || {
+            println!("开始");
+            for i in rx.iter() {
+                let mut vec: Vec<T> = i.clone();
+                let mut array_arr = vec![];
+                loop {
+                    let mut channel_number_clone = channel_number;
+                    if vec.len() < channel_number as usize {
+                        channel_number_clone = vec.len() as i32;
+                    }
+                    let vec_first_10: Vec<T> =
+                        vec.drain(0..channel_number_clone as usize).collect();
+                    println!("{:?}", vec_first_10);
+                    for i in vec_first_10.clone() {
+                        let sum = sum.clone();
+                        let s = thread::spawn(move || {
+                            let mut total_sum = sum.lock().unwrap();
+                            *total_sum = *total_sum + i;
+                        });
+                        array_arr.push(s);
+                    }
+
+                    if vec.is_empty() {
+                        break;
+                    }
+                }
+                for i in array_arr {
+                    i.join().unwrap();
+                }
+                println!("结束了");
+            }
+        });
+        let mut data = Self {
+            bing_fa,
+            tx,
+            json_hand,
+        };
+        data
+    }
+    fn set_data_vec(&mut self, vec_change: Vec<T>) {
+        self.tx.send(vec_change).unwrap();
+    }
+    fn end(self) -> BingFa<T> {
+        let BingFaV2 {
+            json_hand,
+            tx,
+            bing_fa,
+        } = self;
+        drop(tx);
+        json_hand.join().expect("swswsd");
+
+        bing_fa
+    }
+    pub fn clear(&mut self) {
+        self.bing_fa.clear();
+    }
+    pub fn get_vec(&self) -> Vec<T> {
+        self.bing_fa.get_vec()
+    }
+    pub fn get_sum(&self) -> T {
+        self.bing_fa.get_sum()
+    }
+    pub fn get_vec_channel(&self) -> i32 {
+        self.bing_fa.get_vec_channel()
+    }
+    pub fn get_history(&self) -> Vec<T> {
+        self.bing_fa.history.clone()
+    }
+}
+//   pub fn set_data_vec_copy(&mut self, vec_chan
+
+fn main() {
+    let vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let mut bing_fa = BingFaV2::new(vec, 3);
+    println!("{:?}", bing_fa);
+    bing_fa.set_data_vec(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    bing_fa.set_data_vec(vec![9, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    bing_fa.set_data_vec(vec![8, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    bing_fa.set_data_vec(vec![7, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    // thread::sleep(Duration::from_secs(3));
+    println!("{:?}", bing_fa);
+    // bing_fa.end();
+
+    let s = bing_fa.end();
+    println!("{:?}", s);
+    // drop(bing_fa);
+}
