fb284b7d0d8bc2d7313a0f38482f9f477264afb0
hearsay316 <hearsay316@gmail.com>
2024-12-07 22:08:27 +0800

    feat(examples): 添加读取文件夹计数功能
    - 新增读取文件夹计数的示例程序
    - 实现了递归遍历文件夹并统计文件数量的功能
    - 使用命令行参数传递文件夹路径
    - 示例程序位于 examples/读取文件夹计数.rs

---
+++ examples/读取文件夹计数.rs
@@ -0,0 +1,37 @@
+use std::{env, fs};
+
+struct FileNum {
+    path: String,
+}
+impl FileNum {
+    fn from(path: Vec<String>) -> i64 {
+        if path.len() < 2 {
+            panic!("参数错误");
+        };
+        let path = path[1].clone();
+        update_path_name_suffix(&path, 0)
+    }
+}
+fn update_path_name_suffix(path: &str, num: i64) -> i64 {
+    let mut num = num;
+    let entries = fs::read_dir(path).expect("读取文件夹失败");
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let file_path = entry.path();
+            if file_path.is_file() {
+                num += 1;
+            } else {
+                let file_path = file_path.to_string_lossy().to_string();
+                let sum_num = update_path_name_suffix(&file_path, 0);
+                num += sum_num;
+            }
+        }
+    }
+    num
+}
+fn main() {
+    let vec = env::args().collect::<Vec<_>>();
+    println!("{:?}", vec);
+    let num = FileNum::from(vec);
+    println!("{}", num);
+}
