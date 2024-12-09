374768a37d5e6efb3014094b3580bcb8b840fcac
hearsay316 <hearsay316@gmail.com>
2024-12-09 22:44:06 +0800

    refactor(进程通讯):调整输出结果处理方式
    - 移除注释，打印返回数据
    - 使用 split_terminator 替代 split 进行字符串分割
    - 修改数组打印方式，仅显示长度

--- examples/进程通讯.rs
+++ examples/进程通讯.rs
@@ -8,9 +8,9 @@ fn main() {
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
     if output.status.success() {
         let s = String::from_utf8_lossy(&output.stdout);
-        // print!("返回数据:{}", s);
+        print!("返回数据:{}", s);
         let arr = s.split_terminator("\n").collect::<Vec<&str>>();
-        println!("{:?}", arr);
+        println!("{:?}", arr.len());
     } else {
         let s = String::from_utf8_lossy(&output.stderr);
         print!("rustc failed and stderr was:\n{}", s);
