febbbbc97eb67f23301944b4903c1c6649cd2e41
hearsay316 <hearsay316@gmail.com>
2024-12-14 23:27:39 +0800

    refactor(examples): 更新 111.js 示例并添加同步递归示例
    - 注释掉原有的 symlink 代码块
    - 添加新的异步递归函数 test- 在同步递归示例中添加线程溢出错误提示

--- examples/111.js
+++ examples/111.js
@@ -1,5 +1,14 @@
-const symlink = require("fs").symlink;
+// const symlink = require("fs").symlink;
+//
+// symlink('./a.txt', './b.txt', (err,sss)=>{
+//     console.log("xsxs",err,sss)
+// });
 
-symlink('./a.txt', './b.txt', (err,sss)=>{
-    console.log("xsxs",err,sss)
-});
+async function test(num) {
+    if (num <= 1) {
+        return num
+    }
+    return await test(num - 1) + await test(num - 2)
+}
+
+test(100).then(res => console.log(res)).catch(e => console.log(e))
--- examples/同步递归.rs
+++ examples/同步递归.rs
@@ -21,6 +21,12 @@ fn foo(i: i32) -> i32 {
 
 thread 'main' has overflowed its stack
 error: process didn't exit successfully: `target\debug\examples\异步递归.exe` (exit code: 0xc00000fd, STATUS_STACK_OVERFLOW)
+thread 'main' panicked at examples\异步递归.rs:19:5:
+attempt to add with overflow
+stack backtrace:
+   0: std::panicking::begin_panic_handler
+             at /rustc/f6e511eec7342f59a25f7c0534f1dbea00d01b14\library/std\src\panicking.rs:662
+   1: core::panicking::panic_fmt
 
 
 */
