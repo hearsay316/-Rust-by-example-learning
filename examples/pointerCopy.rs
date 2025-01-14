f801cc4ea7764b056176b3e925ef71267d229915
hearsay316 <hearsay316@gmail.com>
2025-01-15 01:51:18 +0800

    feat: 添加数组排序函数并打印结果
    在 `pointerCopy.rs` 中添加了一个 `sort_arr` 函数，用于对数组进行降序排序，并打印排序后的数组结果。

--- examples/pointerCopy.rs
+++ examples/pointerCopy.rs
@@ -5,4 +5,13 @@ fn main() {
     let mut my_speed_ptr: i32 = 88;
     let my_speed_ptr: *mut i32 = &mut my_speed_ptr;
     println!("{:?}", my_speed_ptr);
+    //  需要被封装成一个函数 ,返回排序后的数组
+    fn sort_arr(mut arr: [i32; 5]) -> [i32; 5] {
+        arr.sort_by(|a, b| b.cmp(a));
+        arr
+    }
+    let arr = sort_arr([5, 3, 8, 1, 2]);
+    println!("{:?}", arr);
 }
+
+// Invalid assistant message: content or tool_calls must be set
